5c4494118883f8036114cf9bc4b01c72ee6676fd
Kevin Barabash <kevinb@khanacademy.org>
2018-08-20 00:14:32 -0400

    add filtering

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@
 import * as React from "react";
 import {StyleSheet, css} from "aphrodite";
 
+import {View} from "@khanacademy/wonder-blocks-core";
+
 import data from "../data/output.json";
 console.log(data);
 
 type Props = {};
 type State = {
     selection: string,
+    filter: string,
 };
 
 export default class App extends React.Component<Props, State> {
@@ -16,52 +19,82 @@ export default class App extends React.Component<Props, State> {
 
         this.state = {
             selection: "javascript/content-library-package/modules/subject-progress.jsx",
+            filter: "",
         };
+    }
+
+    handleChange = (e) => {
+        this.setState({filter: e.target.value});
     }
 
     render() {
         const selectionData = data[this.state.selection];
 
-        return <div className={css(styles.row)}>
-            <div className={css(styles.column)}>
+        return <View style={styles.container}>
+            <View style={styles.column}>
                 <h1>files</h1>
-                <ul>
-                    {Object.keys(data)
-                        .filter(filename => data[filename].type === "file")
-                        .sort((a, b) => 
-                            data[b].results["file-size"] - data[a].results["file-size"])
-                        .map(filename => 
-                            <li key={filename} onClick={() => this.setState({selection: filename})}>
-                                {data[filename].results["file-size"]}: {filename}
-                            </li>
-                        )
-                    }
-                </ul>
-            </div>
-            <div className={css(styles.column)}>
+                <View style={styles.row}>
+                    Filter: <input type="text" onChange={this.handleChange} />
+                </View>
+                <View style={styles.column}>
+                    <ul className={css(styles.list)}>
+                        {Object.keys(data)
+                            .filter(filename => filename.includes(this.state.filter))
+                            .filter(filename => data[filename].type === "file")
+                            .sort((a, b) => 
+                                data[b].results["file-size"] - data[a].results["file-size"])
+                            .map(filename => 
+                                <li 
+                                    key={filename} 
+                                    onClick={() => this.setState({selection: filename})}
+                                    className={css(
+                                        styles.listItem,
+                                        filename === this.state.selection && styles.selectedItem,
+                                    )}
+                                >
+                                    {data[filename].results["file-size"]}: {filename}
+                                </li>
+                            )
+                        }
+                    </ul>
+                </View>
+            </View>
+            <View style={styles.column}>
                 <h1>{this.state.selection}</h1>
                 <ul>
-                    <li>type: {selectionData.type}</li>
-                    <li>
+                    <li className={css(styles.item)}>type: {selectionData.type}</li>
+                    <li className={css(styles.item)}>
                         dependents:
                         <ul>
                             {selectionData.dependents.map((dep, i) => 
-                                <li key={i}>{dep}</li>)}
+                                <li 
+                                    key={i}
+                                    className={css(styles.listItem)}
+                                    onClick={() => this.setState({selection: dep})}
+                                >
+                                    {dep}
+                                </li>)}
                         </ul>
                     </li>
-                    <li>
+                    <li className={css(styles.item)}>
                         dependencies:
                         <ul>
                             {selectionData.dependencies.map((dep, i) => 
-                                <li key={i}>{dep}</li>)}
+                                <li 
+                                    key={i}
+                                    className={css(styles.listItem)}
+                                    onClick={() => this.setState({selection: dep})}
+                                >
+                                    {dep}
+                                </li>)}
                         </ul>
                     </li>
-                    <li>
+                    <li className={css(styles.item)}>
                         results:
                         <ul>
                             {Object.entries(selectionData.results)
                                 .map(([name, value]) => 
-                                    <li key={name}>
+                                    <li key={name} className={css(styles.item)}>
                                         {name}: {value.toString()}
                                     </li>
                                 )
@@ -69,18 +102,46 @@ export default class App extends React.Component<Props, State> {
                         </ul>
                     </li>
                 </ul>
-            </div>
-        </div>
+            </View>
+        </View>
     }
 }
 
 const styles = StyleSheet.create({
-    row: {
-        display: "flex",
+    container: {
+        height: "100%",
         flexDirection: "row",
+    },
+    row: {
+        flexDirection: "row",
+        alignItems: "center",
+        flexShrink: 0,
     },
     column: {
         display: "flex",
         flexDirection: "column",
+        overflow: "auto",
+    },
+    list: {
+        listStyleType: "none",
+        padding: 0,
+    },
+    item: {
+        fontFamily: "sans-serif",
+        padding: 2,
+    },
+    listItem: {
+        fontFamily: "sans-serif",
+        padding: 4,
+        cursor: "pointer",
+        ":hover": {
+            background: "lightgray",
+        },
+    },
+    selectedItem: {
+        background: "yellow",
+        ":hover": {
+            background: "yellow",
+        },
     },
 });
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import * as ReactDOM from "react-dom";
 import App from "./app.js";
 
 const container = document.createElement("container");
+container.style.height = "100%";
 document.body.appendChild(container);
 
 ReactDOM.render(<App />, container);
